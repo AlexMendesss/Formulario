0628e96f1351f2fbcd304b3539c373ed1336821b
AlexMendesss <alex.silvacarvalho015@gmail.com>
2022-02-07 23:51:51 -0300

    Criação do PHP

---
+++ cadastro.php
@@ -0,0 +1,11 @@
+<?php
+    if (isset($_POST['enviar'])) {
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $email = $_POST['email'];
+        $area = $_POST['devweb'];
+        $senioridade = $_POST['senioridade'];
+        $tecnologia = $_POST['tecnologia'];
+        $experiencia =$_POST['experiencia'];
+    }
+?>
---
+++ conexao.php
@@ -0,0 +1,3 @@
+<?php
+    mysqli_connect('localhost', 'root', '1020', 'formulario_ballerini') or die("Falha ao conectar-se com a Base de Dados". mysqli_error());
+?>
