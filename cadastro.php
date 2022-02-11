c530654f60b3dc5356f027675f0a0b77c59efb8d
AlexMendesss <alex.silvacarvalho015@gmail.com>
2022-02-11 10:27:02 -0300

    Atualização

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,5 @@
 <?php
+include ('conexao.php');
     if (isset($_POST['enviar'])) {
         $nome = $_POST['nome'];
         $sobrenome = $_POST['sobrenome'];
@@ -7,5 +8,17 @@
         $senioridade = $_POST['senioridade'];
         $tecnologia = $_POST['tecnologia'];
         $experiencia =$_POST['experiencia'];
+        
+        foreach ($tecnologia as $dados_tec){
+            $sql = "insert into cadastro_dev(nome, sobrenome, email, atuacao, senioridade, linguagem, experiencia) values('".$nome."', '".$sobrenome."', '".$email."', '".$area."', '".$senioridade."', '".$dados_tec."', '".$experiencia."')";
+
+            $query = mysqli_query($conectar, $sql);
+        }
+
+        if ($query) {
+            echo ("Cadastro feito com sucesso!!");
+        }else{
+            echo ("Erro ao cadastrar!");
+        }
     }
 ?>
